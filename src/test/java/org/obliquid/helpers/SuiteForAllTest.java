633923551a73ed117d6f6e9ad32e3d84e7f9d5de
Stefano Locati <stefano@obliquid.it>
2011-06-23 02:08:42 +0800

    Added SimpleStopWatchShould.class to the test suite

--- src/test/java/org/obliquid/helpers/SuiteForAllTest.java
+++ src/test/java/org/obliquid/helpers/SuiteForAllTest.java
@@ -1,13 +1,14 @@
 package org.obliquid.helpers;
 
 import org.obliquid.helpers.DateHelperShould;
+import org.obliquid.helpers.SimpleStopWatchShould;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses(value = { DateHelperShould.class })
+@SuiteClasses(value = { DateHelperShould.class, SimpleStopWatchShould.class })
 public class SuiteForAllTest {
 
     public SuiteForAllTest() {
