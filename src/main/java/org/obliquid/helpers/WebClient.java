9a6fbe91fd1e9d0745fbca3ba3fde4ad66f6694e
Stefano Locati <stefano@obliquid.it>
2011-06-23 23:27:58 +0800

    new class WebClient

---
+++ src/main/java/org/obliquid/helpers/WebClient.java
@@ -0,0 +1,32 @@
+package org.obliquid.helpers;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Return info about the Web Client: returnIpAddress()
+ * 
+ * @author stivlo
+ * 
+ */
+public class WebClient {
+
+    private WebClient() {
+        //for the moment there is no reason to allow instances of this class
+    }
+
+    /**
+     * Return the client IP address. In case the address is localhost, "82.96.97.90" is returned
+     * instead, to allow testing.
+     * 
+     * @param request
+     * @return the client IP address
+     */
+    public static String returnIpAddress(final HttpServletRequest request) {
+        String address = request.getRemoteAddr();
+        if (address.equals("0:0:0:0:0:0:0:1")) {
+            //address = "";
+        }
+        return address;
+    }
+
+}
