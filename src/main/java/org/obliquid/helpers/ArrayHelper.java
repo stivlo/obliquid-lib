f942ced0d0b652270f01b80c7ce720e240439efd
Stefano Locati <stefano@obliquid.it>
2011-06-23 19:19:18 +0800

    Added new class ArrayHelper

---
+++ src/main/java/org/obliquid/helpers/ArrayHelper.java
@@ -0,0 +1,104 @@
+package org.obliquid.helpers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Helper class for Arrays and Collections: build HashMap from two arrays, build an ArrayList from
+ * one or two Arrays
+ * 
+ * @author stivlo
+ * 
+ */
+public class ArrayHelper {
+
+    /**
+     * Build an ArrayList from a HashMap.
+     * 
+     * @param param
+     *            HashMap with values to be used to populate our ArrayList
+     * @return an ArrayList Container
+     */
+    public static ArrayList<Object> buildArrayList(final Map<String, Object> param) {
+        ArrayList<Object> arList = new ArrayList<Object>();
+        Iterator<Map.Entry<String, Object>> it = param.entrySet().iterator();
+        while (it.hasNext()) {
+            arList.add((it.next()).getValue());
+        }
+        return arList;
+    }
+
+    /**
+     * Builds an ArrayList from an array of int.
+     * 
+     * @param param
+     *            an array of int
+     * @return an ArrayList Container populated with Integer Objects.
+     */
+    public static ArrayList<Object> buildArrayList(final int[] param) {
+        ArrayList<Object> arList = new ArrayList<Object>();
+        for (int i = 0; i < param.length; i++) {
+            arList.add(param[i]);
+        }
+        return arList;
+    }
+
+    /**
+     * Builds an ArrayList from two HashMap. First all elements from param1 are added, after all
+     * elements from param2.
+     * 
+     * @param param1
+     *            HashMap with values to be used to populate our ArrayList
+     * @param param2
+     *            HashMap with values to be used to populate our ArrayList
+     * @return an ArrayList Container
+     */
+    public static ArrayList<Object> buildArrayList(final Map<String, Object> param1,
+            final Map<String, Object> param2) {
+        ArrayList<Object> arList = new ArrayList<Object>();
+        Iterator<Map.Entry<String, Object>> it;
+        it = param1.entrySet().iterator();
+        while (it.hasNext()) {
+            arList.add((it.next()).getValue());
+        }
+        it = param2.entrySet().iterator();
+        while (it.hasNext()) {
+            arList.add((it.next()).getValue());
+        }
+        return arList;
+    }
+
+    /**
+     * Build an ArrayList from an array of String.
+     * 
+     * @param param
+     *            an Array of String
+     * @return an ArrayList Container populated with String Objects.
+     */
+    public static ArrayList<Object> buildArrayList(final String[] param) {
+        ArrayList<Object> arList = new ArrayList<Object>();
+        for (int i = 0; i < param.length; i++) {
+            arList.add(param[i]);
+        }
+        return arList;
+    }
+
+    /**
+     * Build an HashMap from two arrays, the first one is an array with keys, the second one with
+     * values. The two arrays must have the same size. No check is done for now.
+     * 
+     * @param key
+     * @param val
+     * @return an HashMap
+     */
+    public static HashMap<String, String> buildHashMap(final String[] key, final String[] val) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < key.length; i++) {
+            map.put(key[i], val[i]);
+        }
+        return map;
+    }
+
+}
