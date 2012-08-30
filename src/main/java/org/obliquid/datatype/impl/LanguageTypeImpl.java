4aaf780bb7ef53eddedb5794ad8111e277fc85a5
stivlo <stefano@obliquid.it>
2012-08-30 23:06:45 +0100

    upgrading aws sdk, added javax.mail, fixed 3 unit tests that were failing

--- src/main/java/org/obliquid/datatype/impl/LanguageTypeImpl.java
+++ src/main/java/org/obliquid/datatype/impl/LanguageTypeImpl.java
@@ -15,107 +15,112 @@ import com.google.common.collect.ImmutableMap;
  */
 public class LanguageTypeImpl implements LanguageType {
 
-        /**
-         * Universal serial identifier.
-         */
-        private static final long serialVersionUID = 1L;
+	/**
+	 * Universal serial identifier.
+	 */
+	private static final long serialVersionUID = 1L;
 
-        /**
-         * Supported languages with their translation in the language itself.
-         */
-        private ImmutableMap<String, String> supported = ImmutableMap.of("en", "English", "it", "Italiano");
+	/**
+	 * Supported languages with their translation in the language itself.
+	 */
+	private ImmutableMap<String, String> supported = ImmutableMap.of("en", "English", "it",
+			"Italiano");
 
-        /**
-         * String strategy implementation.
-         */
-        private StringStrategy stringStrategy = new StringStrategy();
+	/**
+	 * String strategy implementation.
+	 */
+	private StringStrategy stringStrategy = new StringStrategy();
 
-        /**
-         * Check whether the languageString is one of the supported languages.
-         * 
-         * @param languageString
-         *                the two letters ISO abbreviation.
-         * @return true if the languageString is valid
-         */
-        @Override
-        public final boolean isValid(final String languageString) {
-                return supported.containsKey(languageString);
-        }
+	/**
+	 * Check whether the languageString is one of the supported languages.
+	 * 
+	 * @param languageString
+	 *            the two letters ISO abbreviation.
+	 * @return true if the languageString is valid
+	 */
+	@Override
+	public final boolean isValid(final String languageString) {
+		if (languageString == null) {
+			return false;
+		}
+		return supported.containsKey(languageString.toLowerCase());
+	}
 
-        /**
-         * Check whether the languageString is one of the supported languages.
-         * Alias for isValid().
-         * 
-         * @param languageString
-         *                the two letters ISO abbreviation.
-         * @return true if the languageString is valid
-         */
-        @Override
-        public final boolean isTheStringValid(final String languageString) {
-                return isValid(languageString);
-        }
+	/**
+	 * Check whether the languageString is one of the supported languages. Alias
+	 * for isValid().
+	 * 
+	 * @param languageString
+	 *            the two letters ISO abbreviation.
+	 * @return true if the languageString is valid
+	 */
+	@Override
+	public final boolean isTheStringValid(final String languageString) {
+		return isValid(languageString);
+	}
 
-        /**
-         * Convert the languageString in the language name in the language
-         * itself. Example: it -> Italiano, en -> English.
-         * 
-         * @param locale
-         *                the locale is ignored because each language name is
-         *                shown in the same language.
-         * @return the full language name such as "Italiano" or "English"
-         * @throws IllegalStateException
-         *                 if language wasn't set
-         */
-        @Override
-        public final String formatData(final Locale locale) throws IllegalStateException {
-                return supported.get(stringStrategy.getData());
-        }
+	/**
+	 * Convert the languageString in the language name in the language itself.
+	 * Example: it -> Italiano, en -> English.
+	 * 
+	 * @param locale
+	 *            the locale is ignored because each language name is shown in
+	 *            the same language.
+	 * @return the full language name such as "Italiano" or "English"
+	 * @throws IllegalStateException
+	 *             if language wasn't set
+	 */
+	@Override
+	public final String formatData(final Locale locale) throws IllegalStateException {
+		return supported.get(stringStrategy.getData());
+	}
 
-        /**
-         * Return the language two chars code.
-         * 
-         * @return the language code
-         * @throws IllegalStateException
-         *                 if the language wasn't set
-         */
-        @Override
-        public final String getData() throws IllegalStateException {
-                return stringStrategy.getData();
-        }
+	/**
+	 * Return the language two chars code.
+	 * 
+	 * @return the language code
+	 * @throws IllegalStateException
+	 *             if the language wasn't set
+	 */
+	@Override
+	public final String getData() throws IllegalStateException {
+		return stringStrategy.getData();
+	}
 
-        /**
-         * Set the two chars language code.
-         * 
-         * @param languageString
-         *                the two chars ISO language code.
-         * @throws IllegalArgumentException
-         *                 if the language isn't one of the accepted ones.
-         */
-        @Override
-        public final void setData(final String languageString) throws IllegalArgumentException {
-                if (!isValid(languageString)) {
-                        throw new IllegalArgumentException("The language code '" + languageString
-                                        + "' isn't valid");
-                }
-                stringStrategy.setDataFromString(languageString);
-        }
+	/**
+	 * Set the two chars language code.
+	 * 
+	 * @param languageString
+	 *            the two chars ISO language code.
+	 * @throws IllegalArgumentException
+	 *             if the language isn't one of the accepted ones.
+	 */
+	@Override
+	public final void setData(final String languageString) throws IllegalArgumentException {
+		if (!isValid(languageString)) {
+			throw new IllegalArgumentException("The language code '" + languageString
+					+ "' isn't valid");
+		}
+		stringStrategy.setDataFromString(languageString);
+	}
 
-        /**
-         * Alias for setData.
-         * 
-         * @param languageString
-         *                two letter ISO language code.
-         * @throws IllegalArgumentException
-         *                 if the language is not supported.
-         */
-        @Override
-        public final void setDataFromString(final String languageString) throws IllegalArgumentException {
-                setData(languageString);
-        }
+	/**
+	 * Alias for setData.
+	 * 
+	 * @param languageString
+	 *            two letter ISO language code.
+	 * @throws IllegalArgumentException
+	 *             if the language is not supported.
+	 */
+	@Override
+	public final void setDataFromString(final String languageString)
+			throws IllegalArgumentException {
+		setData(languageString);
+	}
 
-        @Override
-        public final boolean isAssigned() {
-                return stringStrategy.isAssigned();
-        }
+	@Override
+	public final boolean isAssigned() {
+		return stringStrategy.isAssigned();
+	}
 
 }
